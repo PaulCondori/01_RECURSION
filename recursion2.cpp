549c12a5564d097ebbb65028eb47e0f21af0498e
PaulCondori <pscondoric@unjbg.edu.pe>
2026-09-11 17:07:08 -0500

    Completando recursion2

--- recursion2.cpp
+++ recursion2.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
-using namesace std;
-int sumaArreglos(int arreglo[], int ){
+using namespace std;
+int sumaArreglos(int arreglo[], int n){
 	if (n == 0){
 		return 0;
 	}
-	return arreglo[n - 1] + sumaArreglo, n - 1
+	return arreglo[n - 1] + sumaArreglos(arreglo, n - 1);
+}
+int main(){
+	int n;
+
+	cout << "ingresa la cantidad de elementos del arreglo: ";
+	cin >> n;
+
+	int arreglo[n];
+
+	cout << "ingrese los elementos del arreglo: ";
+	for (int i = 0; i < n; i++){
+		cin >> arreglo[i];
+
+	}
+	int resultado = sumaArreglos(arreglo, n);
+	cout << "La suma de los elementos es: " << resultado << endl;
+	return 0;
 }
